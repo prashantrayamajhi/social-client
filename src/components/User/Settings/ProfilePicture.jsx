749a67856f0f56a2db68f85608bc505c69d77f78
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-08-06 14:29:23 +0545

    display profile picture before updating

--- src/components/User/Settings/ProfilePicture.jsx
+++ src/components/User/Settings/ProfilePicture.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 const ProfilePicture = () => {
   const [image, setImage] = useState(null);
   const user = useSelector((state) => state.profile.user);
+  const [displayImage, setDisplayImage] = useState(null);
 
   const dispatch = useDispatch();
   const handleProfilePicture = (e) => {
@@ -12,6 +13,8 @@ const ProfilePicture = () => {
     const formData = new FormData();
     formData.append("image", image);
     dispatch(updateProfilePicture(formData, user._id));
+    setDisplayImage(null);
+    setImage(null);
   };
 
   return (
@@ -21,8 +24,20 @@ const ProfilePicture = () => {
         name="image"
         onChange={(e) => {
           setImage(e.target.files[0]);
+          setDisplayImage(URL.createObjectURL(e.target.files[0]));
         }}
       />
+      {displayImage && (
+        <img
+          src={displayImage}
+          alt=""
+          className="displayImage"
+          onClick={() => {
+            setImage(null);
+            setDisplayImage(null);
+          }}
+        />
+      )}
       <button type="submit" style={{ marginTop: "2rem" }}>
         Update
       </button>
